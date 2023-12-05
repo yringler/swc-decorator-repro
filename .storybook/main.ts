713e9971e04bf058ace0c19874ee32006c4cc05e
Yehuda Ringler <yehuda.ringler@chabad.org>
2023-12-05 10:09:27 -0500

    TRY-ADDON: addon works

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,14 +2,35 @@ import type { StorybookConfig } from "@storybook/web-components-webpack5";
 
 const config: StorybookConfig = {
   stories: ["../src/core/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    {
+      name: 'storybook-addon-swc',
+      options: {
+        enable: true,
+        enableSwcLoader: true,
+        enableSwcMinify: true,
+        swcLoaderOptions: {
+          jsc: {
+            parser: {
+              syntax: 'typescript',
+              tsx: true,
+              decorators: true
+            },
+            transform: {
+              react: {
+                runtime: 'automatic',
+              },
+            },
+          },
+        }
+      },
+    }
+  ],
   framework: {
     name: "@storybook/web-components-webpack5",
-    options: {
-      builder: {
-        useSWC: true
-      }
-    },
+    options: { },
   },
   docs: {
     autodocs: "tag",
